fa626735213e1e1aafdd51a8373f0054ce8cf3d5
MatheusRisatti <risattimatheus@gmail.com>
2023-05-22 07:51:45 -0300

    erros alterados, projeto finalizado

--- chaves_etec/Controllers/HomeController.cs
+++ chaves_etec/Controllers/HomeController.cs
@@ -2,9 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using chaves_etec.Models;
 using chaves_etec.Services;
-
 namespace chaves_etec.Controllers;
-
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
@@ -13,14 +11,21 @@ public class HomeController : Controller
     public HomeController(ILogger<HomeController> logger, IChavesService chavesService)
     {
         _logger = logger;
-        _chavesService = _chavesService;
+        _chavesService = chavesService;
     }
+
 
     public IActionResult Index(string personagem)
     {
         var persos = _chavesService.GetChavesDto();
         ViewData["filter"] = string.IsNullOrEmpty(personagem) ? "all" : personagem;
         return View(persos);
+    }
+
+        public IActionResult Details(int Numero)
+    {
+        var personagens = _chavesService.GetDetailedPersonagem(Numero);
+        return View(personagens);
     }
 
     public IActionResult Privacy()
